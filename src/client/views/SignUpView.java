8b9f9b24cdb839d58b6fdf1bfdd17a0b2124b2c1
Lou Diamond <2233672@slu.edu.ph>
2024-01-28 20:45:47 +0800

    + added: SignUpView.java

---
+++ src/client/views/SignUpView.java
@@ -0,0 +1,10 @@
+package client.views;
+
+import javax.swing.*;
+
+public class SignUpView {
+    private JFormattedTextField SU_phoneNoField;
+    private JTextField SU_usernameField;
+    private JPasswordField SU_passwordField;
+    private JButton SU_signupbtn;
+}
