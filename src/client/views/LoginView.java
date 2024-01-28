3400731b5abfe8a1a59cbbf68e5d10dca891cae6
Lou Diamond <2233672@slu.edu.ph>
2024-01-28 20:45:40 +0800

    + added: LoginView.java

---
+++ src/client/views/LoginView.java
@@ -0,0 +1,9 @@
+package client.views;
+
+import javax.swing.*;
+
+public class LoginView {
+    private JPasswordField LV_passwordField;
+    private JTextField LV_usernameField;
+    private JButton LV_loginbtn;
+}
