5ac5e306adda3191acd2ebae977ab84383e450c5
Lenar R. Domingo <2233894@slu.edu.ph>
2024-02-01 13:31:58 +0800

    + Barebones of DashboardView

---
+++ src/client/views/DashboardView.java
@@ -0,0 +1,14 @@
+package client.views;
+
+import javax.swing.*;
+
+public class DashboardView {
+    private JPanel mainPanel;
+    private JPanel topPanel;
+    private JPanel bottomPanel;
+    private JPanel searchPanel;
+    private JPanel profilePanel;
+    private JTextField textField1;
+    private JButton button1;
+
+}
