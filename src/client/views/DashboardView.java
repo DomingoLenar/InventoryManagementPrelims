cf22726671cf1b684bb9182e2a9e3ddd2076f17b
Lenar R. Domingo <2233894@slu.edu.ph>
2024-02-04 01:27:03 +0800

    + Small Progress in DashboardView

--- src/client/views/DashboardView.java
+++ src/client/views/DashboardView.java
@@ -1,6 +1,7 @@
 package client.views;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class DashboardView {
     private JPanel mainPanel;
@@ -8,7 +9,43 @@ public class DashboardView {
     private JPanel bottomPanel;
     private JPanel searchPanel;
     private JPanel profilePanel;
-    private JTextField textField1;
-    private JButton button1;
+    private JTextField searchField;
+    private JButton profileButton;
+    private JList<String> activityList1;
+    private JList<String> activityList2;
 
+    public DashboardView() {
+
+        DefaultListModel<String> listModel1 = new DefaultListModel<>();
+        listModel1.addElement("Null");
+        listModel1.addElement("Null");
+        listModel1.addElement("Null");
+        activityList1.setModel(listModel1);
+
+        DefaultListModel<String> listModel2 = new DefaultListModel<>();
+        listModel2.addElement("Null");
+        listModel2.addElement("Null");
+        listModel2.addElement("Null");
+        activityList2.setModel(listModel2);
+
+        //searchField.setBorder(null);
+        //profileButton.setBorder(null);
+
+    }
+
+    public static void main(String[] args) {
+        DashboardView dashboardView = new DashboardView();
+
+        JFrame frame = new JFrame("Dashboard");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(dashboardView.mainPanel);
+
+        frame.setResizable(true);
+
+        frame.setMinimumSize(new Dimension(700, 500));
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
