100640c7869300708270d871bedfd4f09f21fdf5
MarvenLuis <2234244@slu.edu.ph>
2024-02-03 23:28:20 +0800

    - Added utilities to be used by the server and client

---
+++ InventoryManagement/src/utility/ItemOrder.java
@@ -0,0 +1,84 @@
+package utility;
+
+public class ItemOrder {
+    private Item item;
+    private String date;
+    private float purPrice;
+
+    /**
+     * Default constructor for ItemOrder.
+     * Initializes item to null, date to an empty string, and purPrice to 0.
+     */
+    public ItemOrder(){
+        item = null;
+        date = "";
+        purPrice = 0;
+    }
+
+    /**
+     * Constructor for ItemOrder with specified parameters.
+     *
+     * @param item The item associated with the order.
+     * @param date The date when the order was made.
+     * @param purPrice The purchase price of the item in the order.
+     */
+    public ItemOrder(Item item, String date, float purPrice){
+        this.item = item;
+        this.date = date;
+        this.purPrice = purPrice;
+    }
+
+    /**
+     * Gets the item associated with the order.
+     *
+     * @return The item associated with the order.
+     */
+    public Item getItem() {
+        return item;
+    }
+
+    /**
+     * Gets the date when the order was made.
+     *
+     * @return The date of the order.
+     */
+    public String getDate() {
+        return date;
+    }
+
+    /**
+     * Gets the purchase price of the item in the order.
+     *
+     * @return The purchase price of the item.
+     */
+    public float getPurPrice() {
+        return purPrice;
+    }
+
+    /**
+     * Sets the item associated with the order.
+     *
+     * @param item The item to be set.
+     */
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    /**
+     * Sets the date of the order.
+     *
+     * @param date The date to be set.
+     */
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    /**
+     * Sets the purchase price of the item in the order.
+     *
+     * @param purPrice The purchase price to be set.
+     */
+    public void setPurPrice(float purPrice) {
+        this.purPrice = purPrice;
+    }
+}
---
+++ InventoryManagement/src/utility/User.java
@@ -0,0 +1,55 @@
+package utility;
+
+
+public class User {
+    private final String username;
+    private final String password;
+
+    /**
+     * Default constructor initializes the user with empty username and password.
+     */
+    public User() {
+        username = "";
+        password = "";
+    }
+
+    /**
+     * Parameterized constructor to create a user with the given username and password.
+     *
+     * @param username The username of the user.
+     * @param password The password of the user.
+     */
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    /**
+     * Gets the username of the user.
+     *
+     * @return The username of the user.
+     */
+    public String getUsername() {
+        return username;
+    }
+
+
+    /**
+     * Gets the password of the user.
+     *
+     * @return The password of the user.
+     */
+    public String getPassword() {
+        return password;
+    }
+
+
+    /**
+     * Calculates the hash code of the user's password.
+     *
+     * @return The hash code of the user's password, or 0 if the password is null.
+     */
+    public int hashPassword() {
+        return password != null ? password.hashCode() : 0;
+    }
+}
