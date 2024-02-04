29cfacac7f50bec78ac27152fa72f8d509ac9a30
Georcelle <2234664@slu.edu.ph>
2024-02-04 14:35:33 +0800

    addes getter methods

--- navigationBar.java
+++ navigationBar.java
@@ -5,7 +5,18 @@ public class navigationBar {
     private JButton profileButton;
     private JButton stockControlButton;
     private JButton dashboardButton;
-
+public JButton getDashboardButton(){
+    return dashboardButton;
+}
+public JButton getFinancesButton(){
+    return financesButton;
+}
+public JButton getProfileButton(){
+    return profileButton;
+}
+public JButton getStockControlButton(){
+    return stockControlButton;
+}
     private void createUIComponents() {
         // TODO: place custom component creation code here 
     }
