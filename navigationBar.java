7799929c210457fa7f8747c7647aee4dcad1115f
Georcelle <2234664@slu.edu.ph>
2024-02-04 14:27:51 +0800

    initial navigation bar

---
+++ navigationBar.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class navigationBar {
+    private JButton financesButton;
+    private JButton profileButton;
+    private JButton stockControlButton;
+    private JButton trackingButton;
+    private JButton dashboardButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here 
+    }
+}
