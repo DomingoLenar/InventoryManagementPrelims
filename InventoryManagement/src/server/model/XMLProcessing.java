50e9aaf38500331e5b84f19e6fc0d281e057077e
Lestat <2233915@slu.edu.ph>
2024-02-06 00:02:53 +0800

    +methods to be used for signup and login operations of the client

--- InventoryManagement/src/server/model/XMLProcessing.java
+++ InventoryManagement/src/server/model/XMLProcessing.java
@@ -1,9 +1,11 @@
 package server.model;
 
 import java.io.File;
+import java.io.PrintWriter;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -19,10 +21,15 @@ public class XMLProcessing {
 
 
 
+    public synchronized static boolean authenticate(User userToAuth){
+        User localUser = findUser(userToAuth.getUsername());
+        assert localUser != null;
+        return localUser.equals(userToAuth);
+    }
 
-    public static User findUser(String userName) {
+    public synchronized static User findUser(String userName) {
         try{
-           File xmlFile = new File("InventoryManagement/src/server/data/users.xml");
+           File xmlFile = new File("InventoryManagement/src/server/res/users.xml");
            DocumentBuilderFactory dbF = DocumentBuilderFactory.newInstance();
            DocumentBuilder dB = dbF.newDocumentBuilder();
            Document doc = dB.parse(xmlFile);
@@ -49,5 +56,53 @@ public class XMLProcessing {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static boolean createUser(User userToCreate){
+        try{
+            File userXML = new File("InventoryManagement/src/server/res/users.xml");
+            DocumentBuilderFactory dBF = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = dBF.newDocumentBuilder();
+            Document document = documentBuilder.parse(userXML);
+
+            Element root = document.getDocumentElement();
+
+            Element newUser = document.createElement("user");
+
+            Element username = document.createElement("username");
+            username.setTextContent(userToCreate.getUsername());
+
+            Element password = document.createElement("password");
+            password.setTextContent(userToCreate.getPassword());
+
+            newUser.appendChild(username);
+            newUser.appendChild(password);
+
+            root.appendChild(newUser);
+
+            writeDOMToFile(root, "InventoryManagement/src/server/res/users.xml" );
+
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+        return true;
+    }
+
+    private static void writeDOMToFile(Node node, String fileName) {
+        try {
+            TransformerFactory transformerFactory =
+                    TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+
+            DOMSource source = new DOMSource(node);
+
+            PrintWriter fileWriter = new PrintWriter(fileName);
+            StreamResult result = new StreamResult(fileWriter);
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            transformer.transform(source, result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
