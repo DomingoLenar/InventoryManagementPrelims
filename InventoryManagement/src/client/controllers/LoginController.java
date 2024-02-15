2f6f195c7f68f21a63ae10d01e651c21aa40322d
Lou <2233672@slu.edu.ph>
2024-02-15 09:26:42 +0800

    + updated: LoginController.java

--- InventoryManagement/src/client/controllers/LoginController.java
+++ InventoryManagement/src/client/controllers/LoginController.java
@@ -1,12 +1,39 @@
 package client.controllers;
 
-import client.InventoryManagementInterface;
 import client.views.LoginView;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class LoginController {
+    InventoryManagementController inventoryManagementController;
     LoginView loginView;
     public LoginController(InventoryManagementController inventoryManagementController){
+        this.inventoryManagementController = inventoryManagementController;
         loginView = new LoginView();
+
+        initComponents();
+    }
+
+    private void initComponents() {
+        initButtons();
+        initTextFields();
+    }
+
+    private void initTextFields() {
+        loginView.getLVusernameField().getText();
+        loginView.getLVpasswordField().getPassword();
+    }
+
+    private void initButtons() {
+        loginView.getLVloginBtn().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                inventoryManagementController.displayMainMenu();
+            }
+        });
+
+
     }
 
     public LoginView getLoginView() {
