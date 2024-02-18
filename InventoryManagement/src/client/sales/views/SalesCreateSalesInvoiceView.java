4f5b282597d316cb178838d969602bc394f9d859
Lou <2233672@slu.edu.ph>
2024-02-18 16:15:24 +0800

    + add: SalesCreateSalesInvoiceView.java(&.form)

---
+++ InventoryManagement/src/client/sales/views/SalesCreateSalesInvoiceView.java
@@ -0,0 +1,25 @@
+package client.sales.views;
+
+import javax.swing.*;
+
+public class SalesCreateSalesInvoiceView {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JButton cancelButton;
+    private JButton createInvoiceButton;
+    private JPanel mainPanel;
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+
+    public JButton getCancelButton() {
+        return cancelButton;
+    }
+
+    public JButton getCreateInvoiceButton() {
+        return createInvoiceButton;
+    }
+}
