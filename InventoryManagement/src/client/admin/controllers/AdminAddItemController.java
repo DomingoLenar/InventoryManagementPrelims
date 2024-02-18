1aceed475a623b451ba067423d7724a4fe474fac
Lou <2233672@slu.edu.ph>
2024-02-18 16:11:23 +0800

    + add: AdminAddItemController class responsible for handling admin adding an item request

---
+++ InventoryManagement/src/client/admin/controllers/AdminAddItemController.java
@@ -0,0 +1,38 @@
+package client.admin.controllers;
+
+import client.admin.views.AdminAddItemView;
+import client.common.controllers.InventoryManagementController;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AdminAddItemController {
+    AdminAddItemView adminAddItemView;
+
+    InventoryManagementController inventoryManagementController;
+    public AdminAddItemController(InventoryManagementController inventoryManagementController) {
+        this.inventoryManagementController = inventoryManagementController;
+        adminAddItemView = new AdminAddItemView();
+        initButtons();
+    }
+
+    private void initButtons() {
+        adminAddItemView.getAddItemButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: send to server
+            }
+        });
+
+        adminAddItemView.getCancelbtn().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                inventoryManagementController.changeScreen(inventoryManagementController.getAdminStockControlController().adminStockControlView.getMainPanel());
+            }
+        });
+    }
+
+    public AdminAddItemView getAdminAddItemView() {
+        return adminAddItemView;
+    }
+}
