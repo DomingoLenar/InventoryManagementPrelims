6aa01751a0e7a24afb5899e04148465aef763820
Lou <2233672@slu.edu.ph>
2024-02-18 16:12:31 +0800

    + add: AdminStockControlController class responsible for handling admin stock control request

---
+++ InventoryManagement/src/client/admin/controllers/AdminStockControlController.java
@@ -0,0 +1,43 @@
+package client.admin.controllers;
+
+import client.admin.views.AdminStockControlView;
+import client.common.controllers.InventoryManagementController;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AdminStockControlController {
+    AdminStockControlView adminStockControlView;
+    InventoryManagementController inventoryManagementController;
+
+    public AdminStockControlController(InventoryManagementController inventoryManagementController) {
+        this.inventoryManagementController = inventoryManagementController;
+        adminStockControlView = new AdminStockControlView();
+
+        initComponents();
+    }
+
+    private void initComponents() {
+        initButtons();
+    }
+
+    private void initButtons() {
+        adminStockControlView.getAddItemButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                inventoryManagementController.changeScreen(inventoryManagementController.getAdminAddItemController().adminAddItemView.getMainPanel());
+            }
+        });
+
+        adminStockControlView.getSalesInvoiceButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                inventoryManagementController.changeScreen(inventoryManagementController.getAdminCreateSalesInvoiceController().adminCreateSalesInvoiceView.getMainPanel());
+            }
+        });
+    }
+
+    public AdminStockControlView getAdminStockControlView() {
+        return adminStockControlView;
+    }
+}
