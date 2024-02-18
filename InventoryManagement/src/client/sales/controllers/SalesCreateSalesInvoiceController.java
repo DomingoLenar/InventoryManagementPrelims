b8c64f93890ff21d08fc0c45a37c3f2fed380e70
Lou <2233672@slu.edu.ph>
2024-02-18 16:13:49 +0800

    + add: SalesCreateSalesInvoiceController class responsible for handling sales creation of sales invoice request

---
+++ InventoryManagement/src/client/sales/controllers/SalesCreateSalesInvoiceController.java
@@ -0,0 +1,38 @@
+package client.sales.controllers;
+
+import client.common.controllers.InventoryManagementController;
+import client.sales.views.SalesCreateSalesInvoiceView;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SalesCreateSalesInvoiceController {
+    SalesCreateSalesInvoiceView salesCreateSalesInvoiceView;
+    InventoryManagementController inventoryManagementController;
+
+    public SalesCreateSalesInvoiceController(InventoryManagementController inventoryManagementController) {
+        this.inventoryManagementController = inventoryManagementController;
+        salesCreateSalesInvoiceView = new SalesCreateSalesInvoiceView();
+        initButtons();
+    }
+
+    private void initButtons() {
+        salesCreateSalesInvoiceView.getCreateInvoiceButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: send to the server
+            }
+        });
+
+        salesCreateSalesInvoiceView.getCancelButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                inventoryManagementController.changeScreen(inventoryManagementController.getSalesStockControlController().getSalesStockControlView().getMainPanel());
+            }
+        });
+    }
+
+    public SalesCreateSalesInvoiceView getSalesCreateSalesInvoiceView() {
+        return salesCreateSalesInvoiceView;
+    }
+}
