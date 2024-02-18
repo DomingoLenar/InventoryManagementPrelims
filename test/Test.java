345bcabf09b81b20a6dfd8dee8defc9287b8831e
Lestat <2233915@slu.edu.ph>
2024-02-18 23:31:03 +0800

    + new test case for continuous use of input and output stream, discovered solution for bug in branch fix/error-2

--- test/Test.java
+++ test/Test.java
@@ -4,13 +4,16 @@ import server.model.XMLProcessing;
 import utility.User;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Test {
     @org.junit.jupiter.api.Test
     public void testCreateUser(){
         User user = new User("lou", "lou", "purchase", true);
-        boolean valid = XMLProcessing.createUser(user);
+        boolean valid = XMLProcessing.createUser(user).isActive();
         Assertions.assertEquals(true, valid);
     }
 
@@ -33,7 +36,7 @@ public class Test {
     public void testClientServerConnection() {
         try {
             Socket cSocket = new Socket("localhost", 2018);
-            String userType = ProfileManagementModel.handleLogin("testuser", "usertest", cSocket);
+            String userType = ProfileManagementModel.handleLogin("testsales", "salestest", cSocket);
 
             if (userType != null) {
                 System.out.println("login success");
@@ -42,6 +45,31 @@ public class Test {
             }
 
         } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testContinuousOperations() {
+        try {
+            Socket cSocket = new Socket("localhost", 2018);
+            ObjectOutputStream oos  = new ObjectOutputStream(cSocket.getOutputStream());
+            ObjectInputStream ois = new ObjectInputStream(cSocket.getInputStream());
+
+            oos.writeUTF("fetchListOfUsers");
+            oos.flush();
+            ArrayList<User> userList = (ArrayList<User>) ois.readObject();
+
+            oos.writeUTF("fetchListOfUsers");
+            oos.flush();
+            ArrayList<User> userList2 = (ArrayList<User>) ois.readObject();
+
+            System.out.println(userList2.size());
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
