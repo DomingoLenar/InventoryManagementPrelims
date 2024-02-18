9d7cd3af23035195d2e61f90b19a82477cdb3bd1
Lou <2233672@slu.edu.ph>
2024-02-18 16:29:02 +0800

    + update: PurchaserNavigationBarController class for handling purchaser navigation request

--- InventoryManagement/src/client/purchaser/controllers/PurchaserNavigationBarController.java
+++ InventoryManagement/src/client/purchaser/controllers/PurchaserNavigationBarController.java
@@ -3,6 +3,9 @@ package client.purchaser.controllers;
 import client.common.controllers.InventoryManagementController;
 import client.purchaser.views.PurchaserNavigationBarView;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class PurchaserNavigationBarController {
     InventoryManagementController inventoryManagementController;
     PurchaserNavigationBarView purchaserNavigationBarView;
@@ -15,6 +18,19 @@ public class PurchaserNavigationBarController {
     }
 
     private void initButtons() {
+        purchaserNavigationBarView.getDashboardButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                inventoryManagementController.changeScreen(inventoryManagementController.getPurchaserDashboardController().getPurchaserDashboardView().getMainPanel());
+            }
+        });
+
+        purchaserNavigationBarView.getProfileButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                inventoryManagementController.changeScreen(inventoryManagementController.getUserSettingsController().getUserSettingsView().getMainPanel());
+            }
+        });
     }
 
     public PurchaserNavigationBarView getPurchaserNavigationBarView() {
