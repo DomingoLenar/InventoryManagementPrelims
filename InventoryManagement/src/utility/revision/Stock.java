ef18110b0a0d41e487801078c957f064abf0ea66
Lestat <2233915@slu.edu.ph>
2024-03-06 17:33:08 +0800

    + getter and setters

--- InventoryManagement/src/utility/revision/Stock.java
+++ InventoryManagement/src/utility/revision/Stock.java
@@ -8,7 +8,7 @@ public class Stock {
 
     public Stock(){
         this.batchNo = null;
-        this.costs = -1;
+        this.cost = -1;
         this.price = -1;
         this.qty = -1;
     }
@@ -17,6 +17,38 @@ public class Stock {
         this.batchNo = batchNo;
         this.cost = cost;
         this.price = price;
+        this.qty = qty;
+    }
+
+    public String getBatchNo() {
+        return batchNo;
+    }
+
+    public void setBatchNo(String batchNo) {
+        this.batchNo = batchNo;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
         this.qty = qty;
     }
 }
