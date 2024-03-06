01207c24b12e6caabdfacdeccfb7c99ed03768de
Lestat <2233915@slu.edu.ph>
2024-03-06 10:58:45 +0800

    + constructors

--- InventoryManagement/src/utility/revision/Stock.java
+++ InventoryManagement/src/utility/revision/Stock.java
@@ -5,4 +5,18 @@ public class Stock {
     private float cost;
     private float price;
     private int qty;
+
+    public Stock(){
+        this.batchNo = null;
+        this.costs = -1;
+        this.price = -1;
+        this.qty = -1;
+    }
+
+    public Stock(String batchNo, float cost, float price, int qty){
+        this.batchNo = batchNo;
+        this.cost = cost;
+        this.price = price;
+        this.qty = qty;
+    }
 }
