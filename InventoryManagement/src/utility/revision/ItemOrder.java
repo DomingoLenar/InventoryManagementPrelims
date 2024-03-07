340b9b422eca82b831dbe7571ade41b78bb7da7d
Lestat <2233915@slu.edu.ph>
2024-03-07 23:58:52 +0800

    + 2nd overhaul of ItemOrder for ItemOrder to support multiple items within one order, it would be used in parallel with new class named OrderDetails

--- InventoryManagement/src/utility/revision/ItemOrder.java
+++ InventoryManagement/src/utility/revision/ItemOrder.java
@@ -3,35 +3,17 @@ package utility.revision;
 import utility.User;
 
 public class ItemOrder {
+
     private User createdBy;
+    private int orderId;
     private String date;
     private String orderType;
-    private int itemId;
-    private String batchNo;
-    private String supplier;
-    private float price;
-    private int qty;
 
-    public ItemOrder() {
-        this.createdBy = null;
-        this.date = null;
-        this.orderType = null;
-        this.itemId = -1;
-        this.batchNo = null;
-        this.supplier = null;
-        this.price = -1;
-        this.qty = -1;
-    }
-
-    public ItemOrder(User createdBy, String date, String orderType, int itemId, String batchNo, String supplier, float price, int qty) {
+    public ItemOrder(User createdBy, int orderId, String date, String orderType) {
         this.createdBy = createdBy;
+        this.orderId = orderId;
         this.date = date;
         this.orderType = orderType;
-        this.itemId = itemId;
-        this.batchNo = batchNo;
-        this.supplier = supplier;
-        this.price = price;
-        this.qty = qty;
     }
 
     public User getCreatedBy() {
@@ -40,6 +22,14 @@ public class ItemOrder {
 
     public void setCreatedBy(User createdBy) {
         this.createdBy = createdBy;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
     }
 
     public String getDate() {
@@ -56,45 +46,5 @@ public class ItemOrder {
 
     public void setOrderType(String orderType) {
         this.orderType = orderType;
-    }
-
-    public int getItemId() {
-        return itemId;
-    }
-
-    public void setItemId(int itemId) {
-        this.itemId = itemId;
-    }
-
-    public String getBatchNo() {
-        return batchNo;
-    }
-
-    public void setBatchNo(String batchNo) {
-        this.batchNo = batchNo;
-    }
-
-    public String getSupplier() {
-        return supplier;
-    }
-
-    public void setSupplier(String supplier) {
-        this.supplier = supplier;
-    }
-
-    public float getPrice() {
-        return price;
-    }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
-
-    public int getQty() {
-        return qty;
-    }
-
-    public void setQty(int qty) {
-        this.qty = qty;
     }
 }
