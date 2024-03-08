11c320c90bc9aa7e2e8c96466e1fe88a1a02f606
MarvenLuis <2234244@slu.edu.ph>
2024-03-09 01:40:37 +0800

    + Refactored the test case for RequestSalesDashboard

--- test/RequestSalesDashboardTest.java
+++ test/RequestSalesDashboardTest.java
@@ -1,139 +1,44 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import server.model.query.RequestSalesDashboard;
 import utility.revision.Item;
-import utility.revision.ItemOrder;
 
-import java.io.*;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class RequestSalesDashboardTest {
 
     @Test
-    public void testProcess() {
+    public void testProcessMethod() {
         try {
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+            Socket cSocket = new Socket("localhost", 2020);
+            ObjectOutputStream oOS = new ObjectOutputStream(cSocket.getOutputStream());
+            ObjectInputStream oIS = new ObjectInputStream(cSocket.getInputStream());
 
-            RequestSalesDashboard.process(objectOutputStream);
+            RequestSalesDashboard.process(oOS);
 
-            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
-            ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+            float[] revenueAndCost = (float[]) oIS.readObject();
+            ArrayList<Item> recentlyAddedItems = (ArrayList<Item>) oIS.readObject();
+            ArrayList<float[]> yearlyRevenueCogs = (ArrayList<float[]>) oIS.readObject();
 
-            float[] revenueAndCost = (float[]) objectInputStream.readObject();
-            ArrayList<Item> recentlyAddedItems = (ArrayList<Item>) objectInputStream.readObject();
-            ArrayList<float[]> yearlyRevenueCogs = (ArrayList<float[]>) objectInputStream.readObject();
+            assertNotNull(revenueAndCost);
+            assertNotNull(recentlyAddedItems);
+            assertNotNull(yearlyRevenueCogs);
 
-            Assertions.assertNotNull(revenueAndCost);
-            Assertions.assertNotNull(recentlyAddedItems);
-            Assertions.assertNotNull(yearlyRevenueCogs);
+            System.out.println("Revenue and Cost for today: " + revenueAndCost[0] + ", " + revenueAndCost[1]);
+            System.out.println("Recently added items: " + recentlyAddedItems);
+            System.out.println("Yearly revenue and costs: " + yearlyRevenueCogs);
 
-        } catch (IOException | ClassNotFoundException e) {
+            cSocket.close();
+
+        } catch (Exception e) {
             e.printStackTrace();
-            Assertions.fail("Exception occurred: " + e.getMessage());
+            fail("Exception occurred: " + e.getMessage());
         }
-    }
-
-    @Test
-    public void testGetYearlyRevenueNCosts() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Class<?> clazz = Class.forName("server.model.query.RequestSalesDashboard");
-        Method method = clazz.getDeclaredMethod("getYearlyRevenueNCosts");
-        method.setAccessible(true);
-        ArrayList<float[]> result = (ArrayList<float[]>) method.invoke(null);
-        System.out.println("Yearly Revenue and Costs:");
-        for (float[] data : result) {
-            System.out.println("Revenue: " + data[0] + ", Cost: " + data[1]);
-        }
-        Assertions.assertNotNull(result);
-
-    }
-
-    @Test
-    public void testGetRevenueCogsByMonth() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, ClassNotFoundException {
-        Class<?> clazz = Class.forName("server.model.query.RequestSalesDashboard");
-        Method method = clazz.getDeclaredMethod("getRevenueCogsByMonth", String[].class, ArrayList.class);
-        method.setAccessible(true);
-        String[] yearAndMonth = {"2024", "03"};
-        ArrayList<ItemOrder> allSalesOrder = new ArrayList<>();
-        float[] result = (float[]) method.invoke(null, yearAndMonth, allSalesOrder);
-        System.out.println("Revenue and Cost by Month for 2024-03:");
-        System.out.println("Revenue: " + result[0] + ", Cost: " + result[1]);
-        Assertions.assertNotNull(result);
-
-    }
-
-    @Test
-    public void testGetRecentlyAddedItems() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, ClassNotFoundException {
-        Class<?> clazz = Class.forName("server.model.query.RequestSalesDashboard");
-        Method method = clazz.getDeclaredMethod("getRecentlyAddedItems");
-        method.setAccessible(true);
-        ArrayList<Item> result = (ArrayList<Item>) method.invoke(null);
-        System.out.println("Recently Added Items:");
-        for (Item item : result) {
-            System.out.println("Item ID: " + item.getId());
-        }
-        Assertions.assertNotNull(result);
-        Assertions.assertFalse(result.isEmpty());
-    }
-
-
-    @Test
-    public void testGetCurrentDate() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, ClassNotFoundException {
-        Class<?> clazz = Class.forName("server.model.query.RequestSalesDashboard");
-        Method method = clazz.getDeclaredMethod("getCurrentDate");
-        method.setAccessible(true);
-        String result = (String) method.invoke(null);
-        System.out.println("Current Date: " + result);
-        Assertions.assertNotNull(result);
-
-    }
-
-    @Test
-    public void testGetSalesToday() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, ClassNotFoundException  {
-        Class<?> clazz = Class.forName("server.model.query.RequestSalesDashboard");
-        Method method = clazz.getDeclaredMethod("getSalesToday");
-        method.setAccessible(true);
-        ArrayList<ItemOrder> result = (ArrayList<ItemOrder>) method.invoke(null);
-        System.out.println("Sales Today:");
-        for (ItemOrder order : result) {
-            System.out.println(order);
-        }
-        Assertions.assertNotNull(result);
-
-    }
-
-    @Test
-    public void testGetRevenueNCostToday() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, ClassNotFoundException {
-        Class<?> clazz = Class.forName("server.model.query.RequestSalesDashboard");
-        Method method = clazz.getDeclaredMethod("getRevenueNCostToday");
-        method.setAccessible(true);
-        float[] result = (float[]) method.invoke(null);
-        System.out.println("Revenue and Cost Today:");
-        System.out.println("Revenue: " + result[0] + ", Cost: " + result[1]);
-        Assertions.assertNotNull(result);
-
-    }
-
-    @Test
-    public void testGetCostOfGoodsSoldToday() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, ClassNotFoundException {
-        Class<?> clazz = Class.forName("server.model.query.RequestSalesDashboard");
-        Method method = clazz.getDeclaredMethod("getCostOfGoodsSoldToday");
-        method.setAccessible(true);
-        float result = (float) method.invoke(null);
-        System.out.println("Cost of Goods Sold Today: " + result);
-
-    }
-
-    @Test
-    public void testGetRevenueToday() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException , ClassNotFoundException{
-        Class<?> clazz = Class.forName("server.model.query.RequestSalesDashboard");
-        Method method = clazz.getDeclaredMethod("getRevenueToday");
-        method.setAccessible(true);
-        float result = (float) method.invoke(null);
-        System.out.println("Revenue Today: " + result);
-
     }
 
 
