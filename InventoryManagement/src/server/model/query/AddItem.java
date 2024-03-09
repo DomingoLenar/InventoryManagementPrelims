50732464731511bd483c309dfc1692ba478a63c0
Lestat <2233915@slu.edu.ph>
2024-03-09 17:28:30 +0800

    + initial commit of AddItem class

---
+++ InventoryManagement/src/server/model/query/AddItem.java
@@ -0,0 +1,13 @@
+package server.model.query;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class AddItem {
+
+    public static void process(ObjectOutputStream objectOutputStream, ObjectInputStream objectInputStream){
+
+    }
+
+
+}
