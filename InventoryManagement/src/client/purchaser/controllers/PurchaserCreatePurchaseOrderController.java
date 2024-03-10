5c765d2d9a817398149bd09d042ccecd748d3b00
Lou <2233672@slu.edu.ph>
2024-03-11 03:06:20 +0800

    + Implement: Creation of purchase order of purchaser user in PurchaserCreatePurchaseOrderController class
    - Added PurchaserCreatePurchaseOrderModel class for processing of creation of purchase order in the server

--- InventoryManagement/src/client/purchaser/controllers/PurchaserCreatePurchaseOrderController.java
+++ InventoryManagement/src/client/purchaser/controllers/PurchaserCreatePurchaseOrderController.java
@@ -1,20 +1,34 @@
 package client.purchaser.controllers;
 
 import client.common.controllers.InventoryManagementController;
+import client.purchaser.models.PurchaserCreatePurchaseOrderModel;
 import client.purchaser.views.PurchaserAddItemView;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class PurchaserCreatePurchaseOrderController {
+    PurchaserCreatePurchaseOrderModel purchaserCreatePurchaseOrderModel;
     InventoryManagementController inventoryManagementController;
     PurchaserAddItemView purchaserAddItemView;
     ObjectOutputStream objectOutputStream;
     ObjectInputStream objectInputStream;
+    String[] productDetails;
+
+    public void setProductDetails(String[] productDetails) {
+        this.productDetails = productDetails;
+    }
+
+    public String[] getProductDetails() {
+        return productDetails;
+    }
+
     public PurchaserCreatePurchaseOrderController(InventoryManagementController inventoryManagementController, ObjectInputStream oIs, ObjectOutputStream oOs){
         this.inventoryManagementController = inventoryManagementController;
+        purchaserCreatePurchaseOrderModel = new PurchaserCreatePurchaseOrderModel();
         purchaserAddItemView = new PurchaserAddItemView();
         objectInputStream = oIs;
         objectOutputStream = oOs;
@@ -30,15 +44,23 @@ public class PurchaserCreatePurchaseOrderController {
         purchaserAddItemView.getAddItemButton().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                int itemId = Integer.parseInt(productDetails[0]);
                 String productName = purchaserAddItemView.getProductField().getText();
-                int productPrice = Integer.parseInt(purchaserAddItemView.getPriceField().getText());
-                int quantity = Integer.parseInt(purchaserAddItemView.getQuantityField().getText());
-
-                // TODO: fix the process of newly added product
-//                ItemManagementModel.addItems(productName, quantity, "null", 6, productPrice, objectOutputStream, objectInputStream);
-//                ItemManagementModel.addItemOrders(6, inventoryManagementController.getFormattedDate(), productPrice, inventoryManagementController.getUserType(), 6,
-//                        inventoryManagementController.getUsername(), 69, objectOutputStream, objectInputStream);
-
+                float purchasePrice = Float.parseFloat(purchaserAddItemView.getPurchasePriceField().getText());
+                int qtyItem = Integer.parseInt(purchaserAddItemView.getQuantityField().getText());
+                String supplier = purchaserAddItemView.getSupplierField().getText();
+                String type = purchaserAddItemView.getTypeField().getText();
+                String createdBy = inventoryManagementController.getInventoryManagementInterface().getUsername();
+                String dateCreated = inventoryManagementController.getInventoryManagementInterface().getFormattedDate();
+                String orderType = inventoryManagementController.getInventoryManagementInterface().getUserType();
+                String batchNo = purchaserAddItemView.getBatchNoField().getText(); // todo: uncertain about the logic
+                boolean creationSuccess = purchaserCreatePurchaseOrderModel.OnCreate(productName, purchasePrice, qtyItem, supplier, type, createdBy, dateCreated, itemId, orderType,
+                        batchNo, objectOutputStream, objectInputStream);
+                if (!creationSuccess) {
+                    JOptionPane.showMessageDialog(null, "Failure to create purchase order", "Creation of Purchase Order Failed", JOptionPane.ERROR_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Purchase order creation success ", "Creation of Purchase Order Success", JOptionPane.INFORMATION_MESSAGE);
+                }
             }
         });
         purchaserAddItemView.getCancelButton().addActionListener(new ActionListener() {
