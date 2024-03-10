84902d42a83aa54d6499d808db3013ec6126abbd
Lou <2233672@slu.edu.ph>
2024-03-11 03:02:30 +0800

    + Update: PurchaserAddItemView class and bound .form for purchaser user
    - Added swing components relevant to purchaser creation of purchase order

--- InventoryManagement/src/client/purchaser/views/PurchaserAddItemView.java
+++ InventoryManagement/src/client/purchaser/views/PurchaserAddItemView.java
@@ -8,10 +8,30 @@ public class PurchaserAddItemView {
     private JPanel mainPanel;
     private JButton addItemButton;
     private JTextField productField;
-    private JTextField priceField;
+    private JTextField pPriceField;
     private JTextField quantityField;
-    private JComboBox warehouseComboBox;
+    private JComboBox warehouseComboBox; // todo: warehouse capability
     private JButton cancelButton;
+    private JTextField typeField;
+    private JTextField supplierField;
+    private JTextField stockLevelField;
+    private JTextField batchNoField;
+
+    public JTextField getBatchNoField() {
+        return batchNoField;
+    }
+
+    public JTextField getStockLevelField() {
+        return stockLevelField;
+    }
+
+    public JTextField getSupplierField() {
+        return supplierField;
+    }
+
+    public JTextField getTypeField() {
+        return typeField;
+    }
 
     public JPanel getMainPanel() {
         return mainPanel;
@@ -25,8 +45,8 @@ public class PurchaserAddItemView {
         return productField;
     }
 
-    public JTextField getPriceField() {
-        return priceField;
+    public JTextField getPurchasePriceField() {
+        return pPriceField;
     }
 
     public JTextField getQuantityField() {
