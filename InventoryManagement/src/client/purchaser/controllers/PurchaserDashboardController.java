f63d474fb6b345bd123c9c800fab11cfd2356b09
Lou <2233672@slu.edu.ph>
2024-03-11 03:37:52 +0800

    + Update: PurchaserDashboardController class for initialization of components

--- InventoryManagement/src/client/purchaser/controllers/PurchaserDashboardController.java
+++ InventoryManagement/src/client/purchaser/controllers/PurchaserDashboardController.java
@@ -1,22 +1,30 @@
 package client.purchaser.controllers;
 
 import client.common.controllers.InventoryManagementController;
+import client.purchaser.models.PurchaserDashboardModel;
 import client.purchaser.views.PurchaserDashboardView;
 
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class PurchaserDashboardController {
+    PurchaserDashboardModel purchaserDashboardModel;
     InventoryManagementController inventoryManagementController;
     PurchaserDashboardView purchaserDashboardView;
+    ObjectInputStream objectInputStream;
+    ObjectOutputStream objectOutputStream;
 
     public PurchaserDashboardController(InventoryManagementController inventoryManagementController, ObjectInputStream oIs, ObjectOutputStream oOs){
+        this.inventoryManagementController = inventoryManagementController;
+        purchaserDashboardModel = new PurchaserDashboardModel();
         purchaserDashboardView = new PurchaserDashboardView();
+        objectInputStream = oIs;
+        objectOutputStream = oOs;
 
-        initComponents();
     }
 
     public void initComponents() {
+        purchaserDashboardModel.fetchDashboard(objectOutputStream, objectInputStream);
 //        String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
         // Change to Raw Data
