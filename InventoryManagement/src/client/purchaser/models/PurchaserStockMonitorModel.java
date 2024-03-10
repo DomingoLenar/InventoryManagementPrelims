bfdb98cbb0227262a70bc8372d57d45d84549e3b
Lou <2233672@slu.edu.ph>
2024-03-11 03:15:39 +0800

    + Add: PurchaserStockMonitorModel class responsible for querying on a server
    - Added fetchItems() method to fetch list of items on the server

--- InventoryManagement/src/client/purchaser/models/PurchaserStockMonitorModel.java
+++ InventoryManagement/src/client/purchaser/models/PurchaserStockMonitorModel.java
@@ -1,4 +1,31 @@
 package client.purchaser.models;
 
+import client.api.ClientApi;
+import utility.revision.Item;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Stack;
+
 public class PurchaserStockMonitorModel {
+
+    public PurchaserStockMonitorModel(){}
+
+    public Stack<Item> fetchItems(ObjectInputStream objectInputStream, ObjectOutputStream objectOutputStream){
+        try {
+            ClientApi.sendAction("fetchItems", objectOutputStream);
+
+            try {
+                Stack<Item> itemStack = (Stack<Item>) objectInputStream.readObject();
+                return itemStack;
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+
+        } catch (IOException ioException) {
+            throw new RuntimeException("Error Fetching Items", ioException);
+        }
+    }
+
 }
