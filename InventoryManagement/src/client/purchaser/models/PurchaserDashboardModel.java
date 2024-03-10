15ba012b4e2436dc80a9fbd3beee597608ad7fbb
Lou <2233672@slu.edu.ph>
2024-03-11 03:36:41 +0800

    + Add: fetchDashboard() method in PurchaserDashboardModel class for querying on the server

--- InventoryManagement/src/client/purchaser/models/PurchaserDashboardModel.java
+++ InventoryManagement/src/client/purchaser/models/PurchaserDashboardModel.java
@@ -1,4 +1,29 @@
 package client.purchaser.models;
 
+import client.api.ClientApi;
+import utility.revision.Item;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
 public class PurchaserDashboardModel {
+    ArrayList<Item> lowStockItems;
+    int[] unitsSold;
+    public PurchaserDashboardModel(){}
+
+    public void fetchDashboard(ObjectOutputStream oOs, ObjectInputStream oIs) {
+        try{
+            ClientApi.sendAction("requestPurchaseDashboard", oOs);
+            try {
+                lowStockItems = (ArrayList<Item>) oIs.readObject();
+                unitsSold = (int[]) oIs.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (IOException ioException) {
+            throw new RuntimeException("Error fetching dashboard datum", ioException);
+        }
+    }
 }
